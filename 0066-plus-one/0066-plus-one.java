a43995c00fe20914b483cf356ebdd62b82a9c678
Himanshu Singh <himanshusingh1177q@gmail.com>
2026-07-10 10:23:32 +0530

    Time: 0 ms (100%), Space: 43.4 MB (61.27%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,6 +1,16 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+        //length of array
         int n=digits.length;
+        //simple logic
+        //loop ulta leke jao
+        //do chize hongi
+        //first arr[index]=9;
+        //ya not equal to 9;
+        //agar not equal to sidha add krke return
+        //nai to uss index ko zero 
+        //if saarre 9 honge to kuch return naii hoga or loop tut jaega
+        //to ek nya array bnao +1 size ka usme pehle index pe 1 krke return
         for(int i=n-1;i>=0;i--)
         {
             if(digits[i]!=9){
