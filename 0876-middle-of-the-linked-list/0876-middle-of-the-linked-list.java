230721e30bc5b47d4b530597448bbfd244ae1418
Himanshu Singh <himanshusingh1177q@gmail.com>
2026-09-03 19:18:04 +0530

    Time: 0 ms (100%), Space: 42.8 MB (53.37%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,37 +10,14 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-         int length=0;
-        int count=0;
-        //to create temp node dont usse
-        //Node temp =new Node(head.val);
-        //this is not a one pass solution
-    //     ListNode temp=head;
-    //     while(temp!=null){
-    //         temp=temp.next;
-    //         length++;
-    //     }
-    //       ListNode temp2=head;
-    //       while(count!=length/2){
-    //         temp2=temp2.next;
-    //         count++;
-    //       }
-    //       return temp2;
-       
-    //     }
-    // }
-//slow and fast approach
-ListNode slow=head;
-ListNode fast=head;
-//condition mai keval fast.next likhenge tbb error aaega
-while(fast != null && fast.next != null)
-{
-    slow=slow.next;
-    fast =fast.next.next;
-}
-return slow;
+        if(head ==null)return null;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+        
     }
 }
-
-        
-    
